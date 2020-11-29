6487c26b0f1ce2974cca2b3ff6bb15e533da9a59
juna <ju0830na17@gmail.com>
2020-11-29 12:26:46 +0900

    Stamp

---
+++ StampTests/StampTests.swift
@@ -0,0 +1,34 @@
+//
+//  StampTests.swift
+//  StampTests
+//
+//  Created by 河本珠奈 on 2020/08/29.
+//  Copyright © 2020 Kawamoto Juna. All rights reserved.
+//
+
+import XCTest
+@testable import Stamp
+
+class StampTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
